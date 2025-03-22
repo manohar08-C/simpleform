702b039084f91ab311ea1095634717f2aa398754
manohar08-C <136872637+manohar08-C@users.noreply.github.com>
2025-03-22 21:44:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.getElementById("userForm").addEventListener("submit", async function(ev
         email: document.getElementById("email").value
     };
 
-    const response = await fetch("http://localhost:5000/submit", {
+    const response = await fetch("http://simpleform.onrender.com/submit", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
