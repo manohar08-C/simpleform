d7873360247f8dfbbc5e4b5d4b7995b82563d3c6
manohar08-C <136872637+manohar08-C@users.noreply.github.com>
2025-03-22 22:02:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: "https://index.html.github.io" }));
 app.use(bodyParser.json());
 
 // Database Connection
