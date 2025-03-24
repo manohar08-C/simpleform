4009bad4598f33ca6b312ce86ef03de6f467c3c7
manohar08-C <136872637+manohar08-C@users.noreply.github.com>
2025-03-24 07:29:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,13 +7,17 @@ const app = express();
 app.use(cors({ origin: "https://index.html.github.io" }));
 app.use(bodyParser.json());
 
+const mysql = require("mysql2");
+require("dotenv").config();
+
 // Database Connection
 const db = mysql.createConnection({
     host: "bikvhywkcxpkhxfkjljy-mysql.services.clever-cloud.com",
     user: "uvu2wx7pxxblo7c4",
     password: "uvu2wx7pxxblo7c4",
     database: "bikvhywkcxpkhxfkjljy",
-    port: 3306
+    port: 3306,
+    connection: "mysql://uvu2wx7pxxblo7c4:IovzZu1h3F3u8UIxAMzn@bikvhywkcxpkhxfkjljy-mysql.services.clever-cloud.com:3306/bikvhywkcxpkhxfkjljy"
 });
 
 db.connect(err => {
@@ -36,6 +40,8 @@ app.post("/submit", (req, res) => {
         res.json({ message: "Data inserted successfully!" });
     });
 });
+
+module.exports = db;
 
 // Start Server
 // app.listen(5000, "0.0.0.0", () => {
